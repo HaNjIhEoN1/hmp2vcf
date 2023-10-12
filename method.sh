a73b51cafbc17f0fec0183d106a51a2445dd19b1
Jiheon <122258679+HaNjIhEoN1@users.noreply.github.com>
2023-10-12 13:37:15 +0900

    Update method.sh

--- method.sh
+++ method.sh
@@ -4,5 +4,4 @@ git clone https://bitbucket.org/tasseladmin/tassel-5-standalone.git
 # hap 2 vcf
 cd 
 cd tassel-5-standalone
-./run_pipeline.pl -Xmx5g -fork1 -h $?.hmp.txt -export
--exportType VCF -runfork1
+./run_pipeline.pl -Xmx5g -fork1 -h $?.hmp.txt -export -exportType VCF -runfork1
